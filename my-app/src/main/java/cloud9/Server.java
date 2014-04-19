1de18df27d4aeac95fb183cce9763aad0aabcf2f
gannicottb <brandon.gannicott@gmail.com>
2014-04-19 02:19:07 -0400

    pkgrun script, changed hbase version to 0.94.7, and stubbed out all queries

--- my-app/src/main/java/cloud9/Server.java
+++ my-app/src/main/java/cloud9/Server.java
@@ -19,57 +19,136 @@ import org.apache.hadoop.conf.Configuration;
 public class Server {
 
 	private static Configuration config;
+	private static String[] serverArgs;
+
+	public static final String HBASE_CONFIGURATION_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
+	public static final String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
 
 	public static void main(String[] args) {
 
-		setPort(80);     
+		serverArgs = args.clone();	//Create a copy of the command line arguments that my handlers can access
 
-		config = HBaseConfiguration.create();
+		setPort(80);	//Listen on port 80 (which requires sudo)
+
+		config = HBaseConfiguration.create();	//Create the HBaseConfiguration
 
 	  get(new Route("/q1") {
 	     @Override
-	     public Object handle(Request request, Response response) {         	
-	        return "cloud9,4897-8874-0242,"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+	     public Object handle(Request request, Response response) {
+	     	String heartbeat = "cloud9,4897-8874-0242,"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());        	
+	     	response.type("text/plain");
+	     	response.header("Content-Length", String.valueOf(heartbeat.length()));
+	      return heartbeat;
 	     }
 	  });
 
-		get(new Route("/test") {
+		get(new Route("/q2") {
+			@Override
+			public Object handle(Request request, Response response) {				        	
+				String result = "";
+				//Q2 stuff here
+				//request.queryParams("userid")
+				//request.queryParams("tweet_time")
+
+				response.type("text/plain");
+				response.header("Content-Length", String.valueOf(result.length()));
+				return result;
+			}
+		});
+
+		get(new Route("/q3") {
+			@Override
+			public Object handle(Request request, Response response) {				        	
+				String result = "";
+				//Q3 stuff here
+				//request.queryParams("userid")				
+				
+				response.type("text/plain");
+				response.header("Content-Length", String.valueOf(result.length()));
+				return result;
+			}
+		});
+
+		get(new Route("/q4") {
+			@Override
+			public Object handle(Request request, Response response) {				        	
+				String result = "";
+				//Q4 stuff here
+				//request.queryParams("time")				
+				
+				response.type("text/plain");
+				response.header("Content-Length", String.valueOf(result.length()));
+				return result;
+			}
+		});
+
+		get(new Route("/q5") {
+			@Override
+			public Object handle(Request request, Response response) {				        	
+				String result = "";
+				//Q5 stuff here
+				//request.queryParams("start_time")
+				//request.queryParams("end_time")
+				//request.queryParams("place")
+				result += request.queryParams("start_time") + "\n"+
+									request.queryParams("end_time") + "\n" +
+									request.queryParams("place");
+				response.type("text/plain");
+				response.header("Content-Length", String.valueOf(result.length()));
+				return result;
+			}
+		});
+
+		get(new Route("/q6") {
+			@Override
+			public Object handle(Request request, Response response) {				        	
+				String result = "";
+				//Q6 stuff here
+				//request.queryParams("userid_min")
+				//request.queryParams("userid_max")
+				
+				response.type("text/plain");
+				response.header("Content-Length", String.valueOf(result.length()));
+				return result;
+			}
+		});
+
+		get(new Route("/test/:table/:family/:qualifier/:row") {
 		 @Override
 		 public Object handle(Request request, Response response) {   
-		 		//String str= getFromHBase("test", "row1"); 
 		 		String result = "";
 		 		try {  
-		 			result = getFromHBase("test", "cf", "a", "row1");
+		 			result = getFromHBase(request.params(":table"), 
+		 														request.params(":family"),
+		 														request.params(":qualifier"),
+		 														request.params(":row"));
 		 		} catch (IOException e){
 		 			System.err.println(e.getMessage());
 		 		} finally {
 		 			return result;
-		 		}
-		 	// 	HTable htable = new HTable();
-		 	// 	try {
-		 	// 		htable = new HTable(config, "test");
-		 	// 	} catch(IOException e){
-				// 	System.err.println(e.getMessage());
-				// }
-		 		
-				// try {
-				// // Use the table as needed, for a single operation and a single thread
-				// 	Result r = htable.get(new Get(new String("row1").getBytes()));
-				// 	result = r.toString();
-				// } catch(IOException e){
-				// 	System.err.println(e.getMessage());
-				// } finally {
-				// 	htable.close();
-				// 	//connection.close(); 
-				// }  	
-		    // return result;
+		 		}		 	
 		 }
 		});
+
+		get(new Route("/args") {
+		 @Override
+		 public Object handle(Request request, Response response) {   
+		 		String result = "";
+		 		for(String s : serverArgs)
+		 		{
+		 			result += s;
+		 		}	
+		 		return result;
+		 }
+		});
+
 
 	}
 
 	private static String getFromHBase(String table, String family, String qualifier, String row) throws IOException{
 		String result = "";
+		//config.set(HBASE_CONFIGURATION_ZOOKEEPER_QUORUM, hbaseZookeeperQuorum);
+		//config.set(HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT, hbaseZookeeperClientPort);
 		//HConnection connection = HConnectionManager.createConnection(config);
 		//HTableInterface htable = connection.getTable(table.getBytes());
 		HTable htable = new HTable(config, table.getBytes());
