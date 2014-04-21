df93620f403a82d9d911f1460ebb313eac417934
gannicottb <brandon.gannicott@gmail.com>
2014-04-21 01:14:16 -0400

    Current implementation of q6 is returning values as expected

--- my-app/src/main/java/cloud9/Server.java
+++ my-app/src/main/java/cloud9/Server.java
@@ -22,6 +22,7 @@ public class Server {
 
 	private static Configuration config;
 	private static String[] serverArgs;
+	private static HTablePool pool;
 	
 	private static String teamHeader = "cloud9,4897-8874-0242";
 
@@ -33,6 +34,7 @@ public class Server {
 
 		setPort(80);	//Listen on port 80 (which requires sudo)
 
+
 		config = HBaseConfiguration.create();	//Create the HBaseConfiguration
 		if(args.length > 0){
 			config.set("hbase.zookeeper.quorum", args[0]+":2181");			
@@ -41,8 +43,8 @@ public class Server {
 			config.set("hbase.regionserver.port", "60020");
 			config.set("hbase.master", args[0]+":9000");
 		}
+			pool = new HTablePool(config, 25);
 
-		//heartbeat = "cloud9,4897-8874-0242,"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
 
 	  	get(new Route("/q1") {
 	     @Override
@@ -130,21 +132,46 @@ public class Server {
 		get(new Route("/q6") {
 			@Override
 			public Object handle(Request request, Response response) {				        	
-				String result = teamHeader+"\n";
-				//Q6 stuff here
-				//request.queryParams("userid_min")
-				//request.queryParams("userid_max")		
-				try{		
-					long startSum = q6Scan("q6", "cf", 
-										request.queryParams("userid_min").getBytes(), 
-										request.queryParams("userid_max").getBytes());
-					long endSum = q6Scan("q6", "cf", 
-										request.queryParams("userid_max").getBytes(), 
-										longToBytes(MAX_UID));
-					//result = String.valueOf(endSum - startSum);
-					result = teamHeader + "\n" + (endSum - startSum);
-				}catch (IOException e){
-		 			System.err.println(e.getMessage());
+				String result = teamHeader+"\n";				
+				long firstSum = 0;
+				long secondSum = 0;		
+				KeyValue[] firstKV;
+				KeyValue[] secondKV;
+				try{
+					Result firstScan = q6Scan("q6", 
+																request.queryParams("userid_min"), 
+																request.queryParams("userid_max"));		
+					firstKV = firstScan.raw();
+					Result secondScan = q6Scan("q6", 
+																request.queryParams("userid_max"), 
+																String.valueOf(60));					
+					secondKV = secondScan.raw();
+					// if(firstKV.length == 2)
+					// {	
+					// 	firstSum = ByteBuffer.wrap(firstKV[1].getValue()).getLong();
+					// }
+					// if(secondKV.length == 2)
+					// {
+					// 	if (new String(secondScan.getRow()).equals(request.queryParams("userid_max"))){
+					// 		secondSum = ByteBuffer.wrap(secondKV[0].getValue()).getLong() + 
+					// 								ByteBuffer.wrap(secondKV[1].getValue()).getLong();
+					// 	}else{
+					// 		secondSum = ByteBuffer.wrap(secondKV[1].getValue()).getLong();
+					// 	}						
+					// }
+					//result += String.valueOf(secondSum - firstSum);
+					// result += ByteBuffer.wrap(firstKV[0].getValue()).getLong();
+					// result += ByteBuffer.wrap(firstKV[1].getValue()).getLong();
+					// result += ByteBuffer.wrap(secondKV[0].getValue()).getLong();
+					// result += ByteBuffer.wrap(secondKV[1].getValue()).getLong();
+					result += new String(firstKV[0].getValue());
+					result += new String(firstKV[1].getValue());
+					result += new String(secondKV[0].getValue());
+					result += new String(secondKV[1].getValue());
+					
+					
+				}catch (Exception e){
+		 			e.printStackTrace();
 		 		} finally {
 		 			response.type("text/plain");
 					response.header("Content-Length", String.valueOf(result.length()));
@@ -163,8 +190,7 @@ public class Server {
 										request.params(":qualifier"),
 										request.params(":start"),
 										request.params(":stop"),
-										true
-										);
+										true);
 			 	} catch (IOException e){
 			 			System.err.println(e.getMessage());
 			 		} finally {
@@ -193,6 +219,21 @@ public class Server {
 		 }
 		});
 
+		get(new Route("/raw/:table/:row") {
+		 @Override
+		 public Object handle(Request request, Response response) {   
+		 		String result = "";
+		 		try {  
+		 			result = rawFromHBase(request.params(":table"),										
+										request.params(":row"));
+		 		} catch (IOException e){
+		 			System.err.println(e.getMessage());
+		 		} finally {
+		 			return result;
+		 		}		 	
+		 }
+		});
+
 		get(new Route("/args") {
 		 @Override
 		 public Object handle(Request request, Response response) {   
@@ -204,16 +245,27 @@ public class Server {
 		 		return result;
 		 }
 		});
+	}
 
+	private static String rawFromHBase(String table, String row) throws IOException{
+		String result = "";			
+		HTableInterface htable = pool.getTable(table);		
+		try {
+			Result r = htable.get(new Get(row.getBytes()));
+			//KeyValue[] result = r.raw();
+			for(KeyValue kv : r.raw()){
+				result += new String(kv.getValue());
+			}
 
+		} finally {
+			htable.close();			
+		}
+		return result;
 	}
 
 	private static String getFromHBase(String table, String family, String qualifier, String row) throws IOException{
-		String result = "";		
-		//HConnection connection = HConnectionManager.createConnection(config);			
-		//HTableInterface htable = connection.getTable(table.getBytes());
-		//connection.getTable() won't compile. Not sure why.
-		HTable htable = new HTable(config, table.getBytes());
+		String result = "";			
+		HTableInterface htable = pool.getTable(table);		
 		try {
 		// Use the table as needed, for a single operation and a single thread
 			Result r = htable.get(new Get(row.getBytes()));
@@ -221,23 +273,26 @@ public class Server {
 
 		} finally {
 			htable.close();
-			//connection.close();
+			
 		}
 		return result;
 	}
 
 	private static String scanFromHBase(String table, String family, String qualifier, String start, String stop, boolean limit) throws IOException{		
 		String output = "";
-		HTable htable = new HTable(config, table.getBytes());
+		HTableInterface htable = pool.getTable(table);	
 		try {		
 			//byte[] startRow = ByteBuffer.allocate(4).putInt(Integer.parseInt(start)).array();
 			//byte[] startRow = intToBytes(Integer.parseInt(start));
 			//byte[] stopRow = intToBytes(Integer.parseInt(stop));
 			//Scan scan = new Scan(startRow, stopRow);
 			Scan scan = new Scan(start.getBytes(), stop.getBytes());
+			//Set some options
 			if(limit){				
 				scan.setFilter(new PageFilter(1));
-			}
+			}			
+			//scan.setReversed(reversed);
+
 			ResultScanner scanResult = htable.getScanner(scan);				
 			Result result = scanResult.next();
 			output += new String(result.getValue(family.getBytes(), 
@@ -245,50 +300,40 @@ public class Server {
 			while(result != null){
 				result = scanResult.next();	
 				output += "\n"+ new String(result.getValue(family.getBytes(), 
-										qualifier.getBytes()));						
-			}			
-			
+									qualifier.getBytes()));						
+			}	
 
 		}catch (IOException e){
  			System.err.println(e.getMessage());
  		} finally {
 			htable.close();			
 			return output;
-		}
-		
+		}		
 	}
 
-	private static long q6Scan(String table, String family, byte[] start, byte[] end) throws IOException{
-		long output = 0;	
-		HTable htable = new HTable(config, table.getBytes());
-		
+	private static Result q6Scan(String table, String start, String end) throws IOException{
+		Result result;
+		HTableInterface htable = pool.getTable(table.getBytes());	
 		try {
-		// Use the table as needed, for a single operation and a single thread
-			
-			//Scan scan = new Scan(start, Arrays.copyOf(end, end.length+1));
-			Scan scan = new Scan(start, end);
-			
-			//scan.setBatch(1);
-			//scan.setFilter(new PageFilter(1));
-
+			Scan scan = new Scan(start.getBytes(), end.getBytes());			
+			scan.setFilter(new PageFilter(1));
 			ResultScanner scanResult = htable.getScanner(scan);	
-			Result result = scanResult.next();
+			result = scanResult.next();			
+			
+			// long count = ByteBuffer.wrap(result.getValue(family.getBytes(), "count".getBytes())).getLong();
+			// long sum = ByteBuffer.wrap(result.getValue(family.getBytes(), "sum".getBytes())).getLong();
 
-			//int x = java.nio.ByteBuffer.wrap(bytes).getInt();
-			long count = ByteBuffer.wrap(result.getValue(family.getBytes(), "count".getBytes())).getLong();
-			long sum = ByteBuffer.wrap(result.getValue(family.getBytes(), "sum".getBytes())).getLong();
-
-			System.out.println("Count:" + count);
-			System.out.println("Sum:" + sum);
-			if(Arrays.equals(result.getRow(), start))
-				output = count + sum;
-			else
-				output = sum;
+			// System.out.println("Count:" + count);
+			// System.out.println("Sum:" + sum);
+			// if(Arrays.equals(result.getRow(), start))
+			// 	output = count + sum;
+			// else
+			// 	output = sum;
 
 		} finally {
 			htable.close();			
 		}
-		return output;
+		return result;
 	}
 
 	public static byte[] longToBytes(long x) {
